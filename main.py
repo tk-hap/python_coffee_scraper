b4dbe5415ec83d469f167e07b717a57074f48846
TK Hapuku <tekaanuh@gmail.com>
2022-10-27 17:20:59 +1300

    cleaned up some functions

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 import sqlite3
 import re
 
-coffee_sites = ['https://folkbrewers.co.nz/products.json', 'https://greyroastingco.com/products.json', 'https://redrabbitcoffee.co.nz/products.json']
+COFFEE_SITES = ['https://folkbrewers.co.nz/products.json', 'https://greyroastingco.com/products.json', 'https://redrabbitcoffee.co.nz/products.json']
 products_json = {}
 products_all = pd.DataFrame()
 
@@ -14,36 +14,64 @@ c = db.cursor()
 db.commit()
 
 def get_json(url):
+    """ 
+    Takes URL and returns JSON data from Shopify products page.
+
+    Parameters:
+    url (str): A website URL ie. https://thisisaurl.com/products.json
+
+    Returns:
+    products_json (dict): Dictionary containing JSON data from the URL
+    """
     # Get data from url using requests
     url_content = requests.get(url)
     # Turn request into json
     products_json = url_content.json()
     return products_json
 
+def create_df(products_json):
+    """
+    Converts JSON dictionary to a pandas dataframe
 
-def transform_df(products_json):
-    json_df = pd.DataFrame.from_dict(products_json['products'])
-    return json_df
+    Parameters:
+    products_json (dict): Dictionary containing JSON Shopify products data
+
+    Returns:
+    products_df (dataframe): Pandas dataframe containing the JSON Shopify products 
+    """
+    products_df = pd.DataFrame.from_dict(products_json['products'])
+    return products_df
+
+def filter_df(products_all):
+    """
+    Filters products dataframe to only include individual coffee products
+
+    Parameters:
+    products_all (dataframe): Unfiltered dataframe containing all Shopify products
+
+    Returns:
+    coffee_df (dataframe): Filtered dataframe
+    
+    """
+    # Conditions to filter data from dataframes
+    cond_coffee = products_all['product_type'] == 'Coffee'
+    cond_notSub = products_all['title'].str.contains('Subscription') == False
+    coffee_df = products_all[cond_coffee & cond_notSub]
+    return coffee_df
 
 
-for url in coffee_sites:
+for url in COFFEE_SITES:
     products_json = get_json(url)
-    products_df = transform_df(products_json)
+    products_df = create_df(products_json)
     products_all = pd.concat([products_all, products_df], ignore_index=True)
     
-# Conditions to filter data from dataframes
-cond_coffee = products_all['product_type'] == 'Coffee'
-cond_notSub = products_all['title'].str.contains('Subscription') == False
 
-coffee_df = products_all[cond_coffee & cond_notSub]
-
+coffee_df = filter_df(products_all)
 # Converts total dataframe to str
 coffee_df = coffee_df.astype(str)
 
 # Converts the ID field back to an int
 coffee_df['id'] = coffee_df['id'].astype(int64)
-
-
 
 coffee_df.to_sql('coffee', db, if_exists='replace', index=False)
 pd.read_sql('select * from coffee', db)
