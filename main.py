15a6d605d5ed3314fd506965597a7606f620540b
Archduke101 <34885571+Archduke101@users.noreply.github.com>
2022-12-21 21:19:30 +1300

    Switch to DynamoDB

--- main.py
+++ main.py
@@ -2,33 +2,40 @@
 from numpy import int64
 import requests
 import pandas as pd
-import sqlite3
-from sqlalchemy import create_engine
-import psycopg2
+import awswrangler as wr
+import boto3
+import pycountry
+from  hashlib import blake2b
 
 COFFEE_SITES = ['https://folkbrewers.co.nz/products.json', 'https://greyroastingco.com/products.json', 'https://redrabbitcoffee.co.nz/products.json']
 products_json = {}
 products_all = pd.DataFrame()
-
-db = create_engine('postgresql+psycopg2://postgres:5a$78Fs#JFt7eG@web-db-prod-01.cqqsz59wf4te.ap-southeast-2.rds.amazonaws.com:5432/postgres')
+dynam_sess = boto3.Session(profile_name='tk-personal')
 
 def get_json(url):
     """ 
     Takes URL and returns JSON data from Shopify products page.
+    Additionally hashes the current data to be added to the dataframe later
 
     Parameters:
     url (str): A website URL ie. https://thisisaurl.com/products.json
 
     Returns:
     products_json (dict): Dictionary containing JSON data from the URL
+    webpage_hashed (str): A hash created from the webpage, used for comparison
     """
+    #Initialize hashing
+    hashing = blake2b(digest_size=10)
     # Get data from url using requests
     url_content = requests.get(url)
+    #Create hash from url data
+    hashing.update(url_content.content)
+    webpage_hashed = hashing.hexdigest()
     # Turn request into json
     products_json = url_content.json()
-    return products_json
+    return products_json, webpage_hashed
 
-def create_df(products_json):
+def create_df(products_json, webpage_hashed):
     """
     Converts JSON dictionary to a pandas dataframe
 
@@ -39,6 +46,7 @@ def create_df(products_json):
     products_df (dataframe): Pandas dataframe containing the JSON Shopify products 
     """
     products_df = pd.DataFrame.from_dict(products_json['products'])
+    products_df.insert(2, "page_hash", webpage_hashed, True)
     return products_df
 
 def filter_df(products_all):
@@ -56,27 +64,33 @@ def filter_df(products_all):
     cond_coffee = products_all['product_type'] == 'Coffee'
     cond_notSub = products_all['title'].str.contains('Subscription') == False
     coffee_df = products_all[cond_coffee & cond_notSub]
-
     return coffee_df
+
+def get_region(row):
+    for country in pycountry.countries:
+        if country.name.lower() in row['handle'].lower():
+            return country.name
 
 
 for url in COFFEE_SITES:
-    products_json = get_json(url)
-    products_df = create_df(products_json)
+    products_json, webpage_hashed = get_json(url)
+    products_df = create_df(products_json, webpage_hashed)
     products_all = pd.concat([products_all, products_df], ignore_index=True)
     
 
 coffee_df = filter_df(products_all)
+# Add region to DF
+coffee_df['region'] = coffee_df.apply(get_region, axis=1)
 # Converts total dataframe to str
 coffee_df = coffee_df.astype(str)
 
 # Converts the ID field back to an int
 coffee_df['id'] = coffee_df['id'].astype(int64)
 
+#extract image url
 coffee_df['images'] = coffee_df['images'].str.extract(r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))", expand=True)[0]
 
-coffee_df.to_sql('coffee', db, if_exists='replace', index=False)
-pd.read_sql('select * from coffee', db)
+wr.dynamodb.put_df(df=coffee_df, table_name="coffee_table", boto3_session=dynam_sess)
 
 
 
