84cc892661bc54e015d2b09edbf5a626c05f3641
TK Hapuku <tekaanuh@gmail.com>
2023-02-19 23:50:22 +1300

    Lambda function for streaming images to s3

---
+++ download-img-function/lambda_function.py
@@ -0,0 +1,20 @@
+import json
+import requests
+import boto3
+
+session = boto3.Session()
+s3 = session.resource('s3')
+bucket_name = 'python-coffee-img'
+bucket = s3.Bucket(bucket_name)
+
+def lambda_handler(event, context):
+    for record in event["Records"]:
+        if record['eventName'] == 'INSERT':
+            image_url = record['dynamodb']['NewImage']['image']['S']
+            roaster = record['dynamodb']['NewImage']['vendor']['S']
+            id = record['dynamodb']['NewImage']['id']['N']
+            key = roaster + "/" + id
+            r = requests.get(image_url, stream=True)
+            
+            bucket.upload_fileobj(r.raw, key)
+            print(f"{image_url} was downloaded")
