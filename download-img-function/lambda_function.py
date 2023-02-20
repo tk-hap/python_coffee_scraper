c33d8d51bcbe3276c1aa5175a725dae61b145ff8
TK Hapuku <tekaanuh@gmail.com>
2023-02-20 23:44:21 +1300

    Add logic to send message to SQS queue containing record ID and new image url

--- download-img-function/lambda_function.py
+++ download-img-function/lambda_function.py
@@ -6,6 +6,7 @@ session = boto3.Session()
 s3 = session.resource('s3')
 bucket_name = 'python-coffee-img'
 bucket = s3.Bucket(bucket_name)
+sqs = boto3.client('sqs')
 
 def lambda_handler(event, context):
     for record in event["Records"]:
@@ -17,6 +18,13 @@ def lambda_handler(event, context):
             r = requests.get(image_url, stream=True)
             
             bucket.upload_fileobj(r.raw, key)
+
+            message = { 'id': id, 'image': "s3://python-coffee-img/" + key}
+            sqs.send_message(
+                QueueUrl = "https://sqs.ap-southeast-2.amazonaws.com/373205127336/coffee-img-queue",
+                MessageBody = json.dumps(message)
+            )
+            
             print(f"{image_url} was downloaded")
 
 # TO DO: Create event filter for "INSERT" events only. Put new image location into sqs for a lambda to update dynamo db. Connect to dynamodb streams Document better :)
