59e2ab4e8948a657d1aa11a79532e9e6a9bf6d7a
TK Hapuku <tekaanuh@gmail.com>
2023-04-03 08:43:59 +1200

    Add query to get roasters in all_vendors

--- test-queries.py
+++ test-queries.py
@@ -2,23 +2,30 @@ import boto3
 from flask import Flask, render_template
 from werkzeug.exceptions import abort
 from boto3.dynamodb.conditions import Key
-dynam_sess = boto3.Session(profile_name='tk-personal')
+dynam_sess = boto3.Session(profile_name='default')
 dynamodb_resource = dynam_sess.resource('dynamodb')
 table = dynamodb_resource.Table('coffee_table')
 
-product_vendor = "ROASTER#Red Rabbit Coffee Co."
-product_id = "ID#6543747252322"
+
+
+
+pk_values = ["Red Rabbit Coffee Co.", "Grey Roasting Co"]
+items = []
+
+all_vendors = table.query(KeyConditionExpression=Key('pk').eq('ROASTER'))['Items']
+
+
+
+roaster_values = [x['sk'] for x in all_vendors]
+latest_products = []
+for roaster in roaster_values:
+        response_batch = table.query(
+            IndexName='latest-index',
+            KeyConditionExpression=Key('pk').eq(roaster),
+            Limit= 1,
+            ScanIndexForward = False
+            
+        )
     
-
-# product = table.query(KeyConditionExpression=Key('pk').eq(product_vendor) & Key('sk').eq(product_id))['Items']
-response = table.query(
-    IndexName='latest-index',
-    KeyConditionExpression=Key('pk').eq({"SS":["ROASTER#Red Rabbit Coffee Co.", "Grey Roasting Co"]}),
-    Limit=1,
-    ScanIndexForward= False
-    )['Items']
-
-
-
-
-print(response)
+        latest_products.append(response_batch['Items'])
+print(latest_products)
